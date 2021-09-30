f0064c0f4844c7c15b1e88fb9ed1b135673de559
Abdul Magomedov <privetabdul@gmail.com>
2021-09-30 11:20:48 +0300

    update script.js, index.html (2 opt)

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
-const newYearsDate = '2021-12-31'
-const newYearsTime = '23:59:59'
-const monthsEl = document.getElementById('months')
+const newYears = '1 Jan 2022'
 const daysEl = document.getElementById('days')
 const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
 
 
-function countdown(date, time) {
-    const today = new Date();
+function countdown() {
+    const newYearsDate = new Date(newYears)
+    const currentDate = new Date()
 
-    const currentMonth = today.getMonth() + 1
-    const currentDate = today.getDate()
-    const currentHours = today.getHours()
-    const currentMinutes = today.getMinutes()
-    const currentSeconds = today.getSeconds()
+    const totalSeconds = (newYearsDate - currentDate) / 1000
 
-    const splitedDate = date.split('-')
-    const splitedTime = time.split(':')
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const mins = Math.floor(totalSeconds % 3600 / 60)
+    const seconds = Math.floor(totalSeconds) % 60
 
-    const parsedMonth = parseInt(splitedDate[1])
-    const parsedDate = parseInt(splitedDate[2])
-    const parsedHours = parseInt(splitedTime[0])
-    const parsedMinutes = parseInt(splitedTime[1])
-    const parsedSeconds = parseInt(splitedTime[2])
-
-    const months = parsedMonth - currentMonth
-    const days = parsedDate - currentDate
-    const hours = parsedHours - currentHours
-    const mins = parsedMinutes - currentMinutes
-    const seconds = parsedSeconds - currentSeconds
-
-    monthsEl.innerHTML = formatDateTime(months)
     daysEl.innerHTML = formatDateTime(days)
     hoursEl.innerHTML = formatDateTime(hours)
     minsEl.innerHTML = formatDateTime(mins)
@@ -42,4 +26,4 @@ function formatDateTime(dateTime) {
     return dateTime >= 10 ? dateTime : '0' + dateTime
 }
 
-setInterval(countdown, 1000, newYearsDate, newYearsTime)
+setInterval(countdown, 1000)
